9ece9869ac8245f14be8f0c7918aacee437ea26c
joonhoekim <26rote@gmail.com>
2026-09-23 15:13:44 +0900

    chore: bump the version to 0.4.1
    Voice fallback and lead-in silence for Linux, plus update and installer assets.

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         in rec {
           nautice = stdenv.mkDerivation {
             pname = "nautice";
-            version = "0.4.0";
+            version = "0.4.1";
             src = ./.;
 
             nativeBuildInputs = [ pkgs.makeWrapper ];
